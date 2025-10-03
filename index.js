4ca4469792cbcde718ddee5b7fb362615360f73a
[usherissleepy] <[ashar.zarrar218@gmail.com]>
2025-10-03 10:33:53 -0400

    tip challenge 3 completed

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Prints the first n Fibonacci numbers
 const printFibonacci = (n) => {
- 
+    let a = 0
+    let b = 1
+    for (let i = 0; i < n; i++) {
+        console.log(a)
+        let next = a + b
+        a = b
+        b = next
+    }
 }
 
-printFibonacci(5);  // should print 0, 1, 1, 2, 3
-printFibonacci(10); // should print 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+printFibonacci(13);  // should print 0, 1, 1, 2, 3
+// printFibonacci(10); // should print 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
